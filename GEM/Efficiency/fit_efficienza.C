c9e0e1a1ce4aefc1eac593fac6d3ce90b3b6f66f
salv0304 <122489370+salv0304@users.noreply.github.com>
2024-12-19 10:43:33 +0100

    Update fit_efficienza.C

--- GEM/Efficiency/fit_efficienza.C
+++ GEM/Efficiency/fit_efficienza.C
@@ -6,13 +6,12 @@
 #include <TCanvas.h>
 
 void fit_efficienza(){
-   //Numero di punti del file da aprire.
+   //Number of points in the data file
   Int_t n = 13;
-  //Vettori ampiezza e conteggio e relativi errori
   Float_t volt[n], efficienza[n], volt_err[n], efficienza_err[n];
 
-  // Legge i dati dal file
-  ifstream in;                			//file in ingresso
+  //Read input file
+  ifstream in;                		
   in.open("efficienza.txt");
   for (Int_t i=0;i<n;i++){
     in >> volt[i] >> volt_err[i] >> efficienza[i] >> efficienza_err[i];
@@ -23,32 +22,31 @@ void fit_efficienza(){
   TGraphErrors *g = new TGraphErrors(n, volt, efficienza, volt_err, efficienza_err);
   		
   g->SetTitle("Efficiency vs Voltage");
-  g->SetMarkerStyle(0);			// marker style (numeri su internet) //0 = nessun marker
-  g->SetMarkerSize(0);    // Aumenta la dimensione dei marker
+  g->SetMarkerStyle(0);			//0 = no marker
+  g->SetMarkerSize(0);   
   g->GetXaxis()->SetTitle("Voltage (V)");
   g->GetYaxis()->SetTitle("Efficiency");
-  g->SetLineColor(kBlack);  // Imposta il colore delle barre di errore (puoi scegliere un colore più visibile)
-  g->SetLineWidth(2);     // Aumenta lo spessore delle barre di errore
+  g->SetLineColor(kBlack);  
+  g->SetLineWidth(2);     
   
-  //Intervalli assi
   g->GetXaxis()->SetRangeUser(3250,3800);
   g->GetYaxis()->SetRangeUser(0,1.3);
     
-    // Definizioe della funzione di fit sigmoide
+    //Definition of the fit function
     TF1 *sigmoid = new TF1("sigmoid", "[0] / (1 + exp(-[1]*(x-[2]))) + [3]", 3250, 3800);
 
-    //guess iniziali
+    //Initial guess
     sigmoid->SetParameters(1, 0.0035,3500 , 0); // Amplitude, Slope, Midpoint, traslazione
     // Fit della funzione // (TFitResultPtr è semplicemente un oggetto che salva i parametri del fit)
     TFitResultPtr fitResult1 = g->Fit(sigmoid, "S"); // "S" per ottenere il risultato del fit 
     
-    // Visualize the fit
+    //Visualize the fit
     TCanvas *c1 = new TCanvas("c1", "Sigmoid Fit");
     g->Draw("AP");
     sigmoid->Draw("same");
     gStyle->SetOptFit(1111);  // Mostra dettagli sul fit nel box delle statistiche
     
-    //Valuto l'efficienza sul plateau:
+    //Evaluate the efficiency on the plateau:
     float midpoint = 3650;
     float y_at_midpoint = sigmoid->Eval(midpoint); // Evaluate the function at x0
     printf("Value of the sigmoid at the midpoint (x = %f): Eff = %f \n", midpoint, y_at_midpoint);
